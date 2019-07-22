63125e4a78a4e96448b112bc67e2804916a8c64c
whiteun <maple1480@naver.com>
2019-07-22 19:17:12 +0900

    Test code 수정중
    ###작업내용###
    - powermock : dependency 설정

--- src/test/java/com/d2/timeline/dao/RepositoryTest.java
+++ src/test/java/com/d2/timeline/dao/RepositoryTest.java
@@ -7,8 +7,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//@RunWith(SpringRunner.class)
+//@DataJpaTest
+//public class RepositoryTest {
+//}
+
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Ignore
 public class RepositoryTest {
 }
-
--- src/test/java/com/d2/timeline/service/RelationServiceTest.java
+++ src/test/java/com/d2/timeline/service/RelationServiceTest.java
@@ -5,25 +5,33 @@ import com.d2.timeline.domain.dao.UserRelationRepository;
 import com.d2.timeline.domain.service.RelationService;
 import com.d2.timeline.domain.vo.Member;
 import com.d2.timeline.domain.vo.UserRelation;
+import org.apache.catalina.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Optional;
 
 import static com.d2.timeline.domain.Constant.RelationServiceConstant.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
+import org.powermock.reflect.internal.WhiteboxImpl;
+
 
 public class RelationServiceTest extends MockTest {
 
     @InjectMocks
     private RelationService relationService;
 
+//    RelationService privateRelationService;
+
     @Mock
     private UserRelationRepository userRelationRepo;
+
     private Member master;
     private Member slave;
     private UserRelation userRelation;
@@ -31,6 +39,7 @@ public class RelationServiceTest extends MockTest {
 
     @Before
     public void setUp() throws Exception {
+        relationService = new RelationService();
         master = Member.builder().id(1L).build();
         slave = Member.builder().id(2L).build();
         relationState = RelationState.REQUEST;
@@ -43,38 +52,46 @@ public class RelationServiceTest extends MockTest {
     }
 
     @Test
-    public void Create_관계생성_성공(){
-
+    public void Create_관계생성_성공() throws Exception {
+        //given
+//        Method createRelation = relationService.getClass().getDeclaredMethod("createRelation", Member.class, Member.class, RelationState.class);
         given(userRelationRepo.save(userRelation)).willReturn(userRelation);
         given(userRelationRepo.findByMasterAndSlave(master, slave)).willReturn(Optional.of(userRelation));
 
-        final UserRelation returnRelation = relationService.createRelation(master, slave, relationState);
+        //when
+        final UserRelation returnRelation = WhiteboxImpl.invokeMethod(relationService, "createRelation", master, slave, relationState);
 
+        //then
         assertEquals(userRelation, returnRelation);
+
     }
 
     @Test
-    public void Delete_관계삭제_성공(){
+    public void Delete_관계삭제_성공() throws Exception {
+        //given
+//        Method deleteRelation = relationService.getClass().getDeclaredMethod("deleteRelation", Member.class, Member.class);
         given(userRelationRepo.existsByMasterAndSlave(master, slave)).willReturn(true);
 
-        final String returnString = relationService.deleteRelation(master, slave);
+        //when
+        final String returnMessage = WhiteboxImpl.invokeMethod(relationService, "deleteRelation", master, slave);
 
-        assertEquals("OK", returnString);
+        //then
+        assertEquals("OK", returnMessage);
     }
-
-    @Test
-    public void Update_관계변경_성공(){
-        final RelationState changeState = RelationState.BLOCK;
-        given(userRelationRepo.findByMasterAndSlave(master, slave)).willReturn(Optional.of(userRelation));
-
-        final String returnString = relationService.updateRelation(master, slave, changeState);
-
-        assertEquals(STATE_CHANGE_OK_MSG, returnString);
-    }
+//
+//    @Test
+//    public void Update_관계변경_성공(){
+//        final RelationState changeState = RelationState.BLOCK;
+//        given(userRelationRepo.findByMasterAndSlave(master, slave)).willReturn(Optional.of(userRelation));
+//
+//        final String returnString = relationService.updateRelation(master, slave, changeState);
+//
+//        assertEquals(STATE_CHANGE_OK_MSG, returnString);
+//    }
 
 
     //TODO 팔로우 요청, 팔로우요청 수락, 팔로우요청 거절, 차단, 차단해제, followerList출력 test code작성해야함
-
+/*
     @Test
     public void 차단_성공(){
 
@@ -96,4 +113,6 @@ public class RelationServiceTest extends MockTest {
                 .build();
 
     }
+
+ */
 }
